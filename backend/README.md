19acfa44c497338e7fe57e29fef965f8bde2aa4d
Evan Mays <evan@rootbeer.computer>
2023-01-21 15:26:19 -0800

    bro server

---
+++ backend/README.md
@@ -0,0 +1,10 @@
+We have starting data in `starting_data/db.json`
+
+You can run on a real life modal server with this command. But you need to set up modal keys
+```
+modal run server.py --app-name "todo_list" --api-call "complete_task(1)"
+```
+
+You can run locally with
+```
+```
--- backend/server.py
+++ backend/server.py
@@ -1,36 +1,46 @@
-import sys
-import pickle
+import json
 import openai
 
 import modal
 
+openai.api_key = "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"
+
 stub = modal.Stub("not-an-api")
 volume = modal.SharedVolume().persist("storage")
 
-class Model:
-    def __enter__(self):
-        with open('db.pickle', 'rb') as handle:
-            self.db = pickle.load(handle)
+image = modal.Image.debian_slim().pip_install("openai")
 
-    @stub.function(
-        shared_volumes={'/': volume},
-    )
-    def api(self, app_name, api_call):
-        gpt3_input = f"""{self.db[app_name]["prompt"]}
+@stub.function(
+    image=image,
+    shared_volumes={'/Users/evan/rizz-ur-api/backend': volume},
+    mounts=[modal.Mount(local_dir="/Users/evan/rizz-ur-api/backend/starting_data", remote_dir="/root")]
+)
+def api(app_name, api_call):
+    db = json.load(open('db.json','r'))
+    gpt3_input = f"""{db[app_name]["prompt"]}
 API Call:
 {api_call}
 
 Database State:
-{self.db[app_name]["state"]}
+{db[app_name]["state"]}
 
 New Database State:
 """
-        new_state = openai.call(gpt3_input)["completion"]
-        self.db[app_name]["state"] = new_state
-        with open('db.pickle', 'wb') as handle:
-            pickle.dump(self.db, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        return "done"
+    completion = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=gpt3_input,
+        temperature=0.0,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    new_state = completion["choices"][0]["text"].strip()
+    print(new_state)
+    db[app_name]["state"] = new_state
+    json.dump(db, open('db.json', 'w'))
+    return "done"
 
 if __name__ == "__main__":
     with stub.run():
-        Model().predict(x)
+        Model().predict("todo_list", "add_task('buy milk")
