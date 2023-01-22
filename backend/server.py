8147481e6ca3139f3e32992011cf703aa8899e99
Evan Mays <evan@rootbeer.computer>
2023-01-21 16:36:56 -0800

    spellbook

--- backend/server.py
+++ backend/server.py
@@ -4,7 +4,17 @@ from flask import Flask
 
 import modal
 
-openai.api_key = "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" # "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"
+import requests
+
+def gpt3(input):
+    response = requests.post(
+    "https://dashboard.scale.com/spellbook/api/app/kw1n3er6",
+    json={
+            "input": input
+        },
+    headers={"Authorization":"Basic cld6n7eoo0065sr1acbwczykv"}
+    )
+    return response.text
 
 stub = modal.Stub("not-an-api")
 volume = modal.SharedVolume().persist("storage")
@@ -15,7 +25,8 @@ def dict_to_json(d):
     return d.__dict__
 
 app = Flask(__name__)
-
+db = json.load(open('db.json','r'))
+print(db['todo_list']["state"])
 # @stub.function(
 #     image=image,
 #     shared_volumes={'/Users/evan/rizz-ur-api/backend': volume},
@@ -24,7 +35,7 @@ app = Flask(__name__)
 @app.route('/<app_name>/<api_call>')
 def api(app_name, api_call):
     db = json.load(open('db.json','r'))
-    print(db)
+    print(db[app_name]["state"])
     gpt3_input = f"""{db[app_name]["prompt"]}
 API Call:
 {api_call}
@@ -34,18 +45,8 @@ Database State:
 
 New Database State (as json):
 """
-    completion = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=gpt3_input,
-        temperature=0.0,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    print("COMPLETION")
-    print(completion["choices"][0]["text"].strip())
-    new_state = json.loads(completion["choices"][0]["text"].strip())
+    completion = gpt3(gpt3_input)
+    new_state = json.loads(json.loads(completion)["text"])
     print("NEW_STATE")
     print(new_state)
     db[app_name]["state"] = new_state
